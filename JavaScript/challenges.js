dc07d7695e2985b7245977f6de7455db90ebb304
mdechavez5 <marcdechavez@gmail.com>
2022-07-15 16:53:42 -0400

    Challenge 19 - Flatten

--- JavaScript/challenges.js
+++ JavaScript/challenges.js
@@ -634,7 +634,18 @@ flatten( [1, [2, [3, [4]]], 1, 'a', ['b', 'c']] );
 //=> [1, 2, 3, 4, 1, 'a', 'b', 'c']
 -----------------------------------------------------------------*/
 // Your solution for 19-flatten here:
-
+function flatten(arr) {
+  var flatArr = [];
+  arr.forEach(function(elem) {
+    // use the Array.isArray static method to test if an array
+    if (Array.isArray(elem)) {
+      flatArr = flatArr.concat(flatten(elem));
+    } else {
+      flatArr.push(elem);
+    }
+  });
+  return flatArr;
+}
 
 
 
