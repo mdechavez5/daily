c5ad064e7c0de4f40f11b9f593de38c6b46ed298
mdechavez5 <marcdechavez@gmail.com>
2022-08-15 09:00:58 -0400

    Challenge 9 - Char Count Refactor

--- JavaScript/challenges.js
+++ JavaScript/challenges.js
@@ -338,19 +338,28 @@ charCount('hello') //=> { h: 1, e: 1, l: 2, o: 1 }
 charCount('Today is fantastic!') //=> { T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 }
 -----------------------------------------------------------------*/
 // Your solution for 09-charCount here:
+// function charCount(str) {
+//   var result = {};
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str.charAt(i);
+//     // already seen this char?
+//     if (result[char]) {
+//       result[char]++;
+//     } else {
+//       result[char] = 1;
+//     }
+//   }
+//   return result;
+// }
+
+/*--- convert str to array and use reduce with a ternary ---*/
 function charCount(str) {
-  var result = {};
-  for (var i = 0; i < str.length; i++) {
-    var char = str.charAt(i);
-    // already seen this char?
-    if (result[char]) {
-      result[char]++;
-    } else {
-      result[char] = 1;
-    }
-  }
-  return result;
+  return str.split('').reduce(function(countObj, char) {
+    countObj[char] = countObj[char] ? ++countObj[char] : 1;
+    return countObj;
+  }, {});
 }
+
 console.log(charCount('hello') )
 console.log(charCount('Today is fantastic!') ) 
 
